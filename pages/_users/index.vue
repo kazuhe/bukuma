0ee76b5f1a00cdcc98ec796d0997794cab192c64
kazuhe <omc.ohara@gmail.com>
2021-04-11 03:03:07 +0900

    add: users/signupページ

---
+++ pages/_users/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>username: {{ username }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: this.$route.params.users
+    }
+  }
+}
+</script>
