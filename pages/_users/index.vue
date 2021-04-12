4e13acd2881f471f0e07aae3243587962fd38866
kazuhe <omc.ohara@gmail.com>
2021-04-13 02:51:31 +0900

    add: asyncDataでAPIから取得したデータをコンポーネントへマージ

--- pages/_users/index.vue
+++ pages/_users/index.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
     <p>username: {{ username }}</p>
+    <img :src="user.profile_image_url" width="120" alt="" />
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <h2>
+          {{ item.title }} <span>by </span>
+          <nuxt-link :to="`/${item.user.id}`">
+            {{ item.user.id }}
+          </nuxt-link>
+        </h2>
+        <div>{{ item.body.slice(0, 130) }}...</div>
+        <a :href="item.url">{{ item.url }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData({ app, route }) {
+    const user = await app.$axios.$get(
+      `https://qiita.com/api/v2/users/${route.params.users}`
+    )
+
+    const items = await app.$axios.$get(
+      `https://qiita.com/api/v2/items?query=user:${route.params.users}`
+    )
+
+    return {
+      user,
+      items
+    }
+  },
+
   data() {
     return {
       username: this.$route.params.users
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Hello world!</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <h2>
+          {{ item.title }} <span>by </span>
+          <nuxt-link :to="`/${item.user.id}`">
+            {{ item.user.id }}
+          </nuxt-link>
+        </h2>
+        <div>{{ item.body.slice(0, 130) }}...</div>
+        <a :href="item.url">{{ item.url }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
+  async asyncData({ app }) {
+    const items = await app.$axios.$get(
+      'https://qiita.com/api/v2/items?query=tag:nuxt.js'
+    )
+
+    return {
+      items
+    }
+  },
+
   async mounted() {
     console.log(
       JSON.stringify(
