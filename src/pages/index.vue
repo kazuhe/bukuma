c781f8f6222eeb05b228401ffb3374e91b7e3a51
kazuhe <omc.ohara@gmail.com>
2021-04-20 11:15:36 +0900

    add: postsページ/loginフォーム

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,40 @@
 <template>
   <div>
-    <h1>Hello world!</h1>
-    <ul>
-      <li v-for="item in items" :key="item.id">
-        <h2>
-          {{ item.title }} <span>by </span>
-          <nuxt-link :to="`/${item.user.id}`">
-            {{ item.user.id }}
-          </nuxt-link>
-        </h2>
-        <div>{{ item.body.slice(0, 130) }}...</div>
-        <a :href="item.url">{{ item.url }}</a>
-      </li>
-    </ul>
+    <Wrapper>
+      <form class="form">
+        <div class="form_item">
+          <label for="username">User id</label>
+          <input
+            id="username"
+            v-model="formData.id"
+            type="text"
+            placeholder=""
+            autocomplete="username"
+          />
+        </div>
+        <div class="form_item">
+          <label for="isCreateMode">アカウントを作成する</label>
+          <input id="isCreateMode" v-model="isCreateMode" type="checkbox" />
+        </div>
+        <div class="button">
+          <button type="button">
+            {{ buttonText }}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
-  async asyncData({ store }) {
-    if (store.getters.items.length) {
-      return
+  asyncData() {
+    return {
+      isCreateMode: false,
+      formData: {
+        id: ''
+      }
     }
-    await store.dispatch('fetchItems')
   },
 
   head() {
@@ -34,7 +44,58 @@ export default {
   },
 
   computed: {
-    ...mapGetters(['items'])
+    buttonText() {
+      return this.isCreateMode ? 'Sign up' : 'Login'
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.form {
+  background: #fcfcfc;
+  padding: 30px;
+  font-size: 14px;
+  border-radius: 5px;
+  box-shadow: 0 15px 30px rgb(50 50 50 / 10%), 0 5px 15px rgb(0 0 0 / 7%);
+
+  &_item {
+    text-align: left;
+
+    label {
+      font-weight: bold;
+      display: block;
+    }
+
+    input[type='text'] {
+      border-radius: 5px;
+      border: none;
+      background: #f2f2f3;
+      padding: 10px 15px;
+      outline: none;
+      margin-top: 5px;
+      width: 100%;
+    }
+
+    & + .form_item {
+      margin: 10px 0 0;
+    }
+  }
+}
+
+.button {
+  button {
+    color: #fff;
+    background: var(--color-primary);
+    padding: 5px 15px;
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 5px;
+    height: 40px;
+    display: inline-block;
+    cursor: pointer;
+    transition: opacity 0.1s;
+    min-width: 150px;
+  }
+}
+</style>
---
+++ src/pages/posts/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <h2>
+          {{ item.title }} <span>by </span>
+          <nuxt-link :to="`/${item.user.id}`">
+            {{ item.user.id }}
+          </nuxt-link>
+        </h2>
+        <div>{{ item.body.slice(0, 130) }}...</div>
+        <a :href="item.url">{{ item.url }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  async asyncData({ store }) {
+    if (store.getters.items.length) {
+      return
+    }
+    await store.dispatch('fetchItems')
+  },
+
+  head() {
+    return {
+      title: 'POSTS'
+    }
+  },
+
+  computed: {
+    ...mapGetters(['items'])
+  }
+}
+</script>
